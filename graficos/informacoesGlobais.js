cc1d32feb9105e7c0fdfea4a11bf6b28f60a3eab
silviosnjr <silviosnjr@gmail.com>
2024-10-31 11:22:27 -0300

    first commit

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,23 @@
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/residencias/residencias-dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasComCasaPropria = (dados.total_pessoas_com_casa_propria / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio_em_casa)
+    const minutos = Math.round((dados.tempo_medio_em_casa - horas) * 60)
+    
+    const porcentagemPessoasComCasaPropria = ((pessoasComCasaPropria / pessoasNoMundo) * 100).toFixed(2);
+
+    const porcentagemTempoEmCasa = ((dados.tempo_medio_em_casa / 24) * 100).toFixed(2);
+    
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas, e que aproximadamente <span>${pessoasComCasaPropria} bilhões</span> possuem casa própria, passando, em média, <span>${horas} horas</span> e <span>${minutos} minutos</span> em casa? <br>Isso significa que aproximadamente <span>${porcentagemPessoasComCasaPropria}%</span> das pessoas possuem uma casa própria e que passam <span>${porcentagemTempoEmCasa}%</span> do seu tempo diário nela.`
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo);
+}
+
+vizualizarInformacoesGlobais();
---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,60 @@
+import { getCSS, tickConfig } from "./common.js"
+
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/residencias/residencias-tipos-de-moradias.json'
+    const rest = await fetch(url)
+    const dados = await rest.json()
+
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios= Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeUsuarios,
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: "Tipos de moradias mais habitadas",
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30,
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: "Tipo de moradia",
+                font: {
+                    color: getCSS('--secundary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: "Quantidade de proprietários",
+                font: {
+                    color: getCSS('--secundary-color')
+                }
+            }
+        }
+    }
+    
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+}
+ 
+quantidadeUsuarios()
