4a130e68b786a445f19140baa1bf1f97444b59f5
silviosnjr <silviosnjr@gmail.com>
2024-10-31 11:28:41 -0300

    inserindo erros

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,12 +3,12 @@ const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGr
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasComCasaPropria = (dados.total_pessoas_com_casa_propria / 1e9)
+    const pessoasComCasaPropria = (dados.total_pessoas_com_casa_propria / 1e8)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio_em_casa)
     const minutos = Math.round((dados.tempo_medio_em_casa - horas) * 60)
     
-    const porcentagemPessoasComCasaPropria = ((pessoasComCasaPropria / pessoasNoMundo) * 100).toFixed(2);
+    const porcentagemPessoasComCasaPropria = ((pessoasComCasaPropria / pessoasNoMundo) * 100);
 
     const porcentagemTempoEmCasa = ((dados.tempo_medio_em_casa / 24) * 100).toFixed(2);
     
