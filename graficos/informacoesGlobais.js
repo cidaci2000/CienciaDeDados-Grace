8e17b17ae6e436b61d3c802cd9e6a0bcc7b52bd7
Aparecida Ferreira <108874543+cidaci2000@users.noreply.github.com>
2024-11-26 19:35:16 +0000

    correto

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,7 +3,7 @@ const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGr
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasComCasaPropria = (dados.total_pessoas_com_casa_propria / 1e8)
+    const pessoasComCasaPropria = (dados.total_pessoas_com_casa_propria / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio_em_casa)
     const minutos = Math.round((dados.tempo_medio_em_casa - horas) * 60)
@@ -14,7 +14,7 @@ async function vizualizarInformacoesGlobais() {
     
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas, e que aproximadamente <span>${pessoasComCasaPropria} bilhões</span> possuem casa própria, passando, em média, <span>${horas} horas</span> e <span>${minutos} minutos</span> em casa? <br>Isso significa que aproximadamente <span>${porcentagemPessoasComCasaPropria}%</span> das pessoas possuem uma casa própria e que passam <span>${porcentagemTempoEmCasa}%</span> do seu tempo diário nela.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas, e que aproximadamente <span>${pessoasComCasaPropria} bilhões</span> possuem casa própria, passando, em média, <span>${horas} horas</span> e <span>${minutos} minutos</span> em casa? <br>Isso significa que aproximadamente <span>${porcentagemPessoasComCasaPropria.toFixed(2)}%</span> das pessoas possuem uma casa própria e que passam <span>${porcentagemTempoEmCasa}%</span> do seu tempo diário nela.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
